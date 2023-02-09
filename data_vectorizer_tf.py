fc4ffd862b480b0dd5209c7df024113682b226d9
Louis Chau <louis321yh@gmail.com>
2023-02-10 00:20:50 +0800

    Added files for the model

--- data_vectorizer_tf.py
+++ data_vectorizer_tf.py
@@ -1,11 +1,14 @@
 # implements the same functions as for data_vectorizer, but with higher efficiency by directly returning
 # indices and tensorflow tensors
-
+import config
 import data
 import data_vectorizer
 import tensorflow as tf
 import numpy as np
 import itertools
+import pandas as pd
+import os
+import json
 
 def transform_string_to_list(x):
     if type(x) == str:
@@ -37,6 +40,75 @@ test_contents_title_vect = tf.ragged.constant(list(test_contents_vect["title_vec
 test_contents_description_vect = tf.ragged.constant(list(test_contents_vect["description_vectorize"]))
 test_topics_title_vect = tf.ragged.constant(list(test_topics_vect["title_vectorize"]))
 test_topics_description_vect = tf.ragged.constant(list(test_topics_vect["description_vectorize"]))
+def obtain_correlations(contents_index, topics_index):
+    contain_contents_list = []
+    lookup_table = pd.Series(index=contents_index, data=range(len(contents_index)))
+    for k in range(len(topics_index)):
+        if k % 100 == 0:
+            print("Load idx:  ",k)
+        topic_id = topics_index[k]
+        if data.topics.loc[topic_id]["has_content"]:
+            content_ids = pd.Index(data.correlations.loc[topic_id]["content_ids"].split())
+            content_ids = list(lookup_table.loc[content_ids.intersection(contents_index)])
+            contain_contents_list.append(content_ids)
+        else:
+            contain_contents_list.append([])
+    return list(contain_contents_list)
+
+if os.path.isfile(config.resources_path + "vectorizer_train_correlations.json"):
+    with open(config.resources_path + "vectorizer_train_correlations.json") as f:
+        train_correlations = json.load(f)
+else:
+    print("First run train correlations: ")
+    train_correlations = obtain_correlations(data_vectorizer.train_contents, data_vectorizer.train_topics)
+    with open(config.resources_path + 'vectorizer_train_correlations.json', 'w') as f:
+        json.dump(train_correlations, f)
+if os.path.isfile(config.resources_path + "vectorizer_test_correlations.json"):
+    with open(config.resources_path + "vectorizer_test_correlations.json") as f:
+        test_correlations = json.load(f)
+else:
+    print("First run test correlations: ")
+    test_correlations = obtain_correlations(data_vectorizer.test_contents, data_vectorizer.test_topics)
+    with open(config.resources_path + 'vectorizer_test_correlations.json', 'w') as f:
+        json.dump(test_correlations, f)
+
+train_correlations_length = np.zeros(shape = (len(train_correlations)), dtype = np.int32)
+test_correlations_length = np.zeros(shape = (len(test_correlations)), dtype = np.int32)
+for k in range(len(train_correlations)):
+    train_correlations[k] = np.array(train_correlations[k], dtype = np.int32)
+    train_correlations_length[k] = len(train_correlations[k])
+train_correlations = np.array(train_correlations, dtype = "object")
+for k in range(len(test_correlations)):
+    test_correlations[k] = np.array(test_correlations[k], dtype = np.int32)
+    test_correlations_length[k] = len(test_correlations[k])
+test_correlations = np.array(test_correlations, dtype = "object")
+
+# asssumes the contents list and topics list are sorted.
+def obtain_correlations_matrix_train(topics_list_train_id, contents_list_train_id):
+    cors = np.zeros(shape = (len(topics_list_train_id), len(contents_list_train_id)))
+    inv_map = np.zeros(shape = (len(data_vectorizer.train_contents)), dtype = np.int32) - 1
+    inv_map[contents_list_train_id] = np.arange(0, len(contents_list_train_id), dtype = np.int32)
+    # np.concatenate(np.train_correlations[topics_list_train_id], axis = 0)
+    # np.repeat(np.array(range(len(topics_list_train_id))), train_correlations_length[topics_list_train_id])
+    k = 0
+    for topic_id in topics_list_train_id:
+        locs = inv_map[train_correlations[topic_id]]
+        cors[k, locs[locs != -1]] = 1
+        k += 1
+    return cors
+
+def obtain_correlations_matrix_test(topics_list_test_id, contents_list_test_id):
+    cors = np.zeros(shape=(len(topics_list_test_id), len(contents_list_test_id)))
+    inv_map = np.zeros(shape=(len(data_vectorizer.test_contents)), dtype=np.int32) - 1
+    inv_map[contents_list_test_id] = np.arange(0, len(contents_list_test_id), dtype=np.int32)
+
+    k = 0
+    for topic_id in topics_list_test_id:
+        locs = inv_map[test_correlations[topic_id]]
+        cors[k, locs[locs != -1]] = 1
+        k += 1
+    return cors
+
 
 def obtain_train_contents_vector(contents_list_train_id):
     return tf.gather(train_contents_title_vect, contents_list_train_id), tf.gather(train_contents_description_vect, contents_list_train_id)
@@ -55,27 +127,23 @@ def obtain_test_topics_vector(topics_list_train_id):
 def random_train_batch_sample(initial_sample_size = 1000, zero_to_one_ratio = None):
     contents_list = np.random.choice(len(data_vectorizer.train_contents), initial_sample_size, replace=False)
     topics_list = np.random.choice(len(data_vectorizer.train_topics), initial_sample_size, replace=False)
-    cor_frame = data_vectorizer.obtain_correlation_frame(list(data_vectorizer.train_topics[topics_list]), list(data_vectorizer.train_contents[contents_list]))
+    cor_mat = obtain_correlations_matrix_train(topics_list, contents_list)
 
     if zero_to_one_ratio is None:
-        prod = itertools.product(list(topics_list), list(contents_list))
-        lists = list(zip(*prod))
-        return list(lists[1]), list(lists[0]), cor_frame.to_numpy().flatten()
+        return np.tile(contents_list, len(topics_list)), np.repeat(topics_list, len(contents_list)), cor_mat.flatten()
 
-    if initial_sample_size < 1000:
+    if initial_sample_size < 800:
         raise Exception("Zero to one ratio can be used only if initial sample size >= 1000!")
 
-    while cor_frame.to_numpy().sum() < 10:
+    while cor_mat.sum() < 10:
         contents_list = np.random.choice(len(data_vectorizer.train_contents), initial_sample_size, replace=False)
         topics_list = np.random.choice(len(data_vectorizer.train_topics), initial_sample_size, replace=False)
-        cor_frame = data_vectorizer.obtain_correlation_frame(list(data_vectorizer.train_topics[topics_list]), list(data_vectorizer.train_contents[contents_list]))
-
-    cor_mat = cor_frame.to_numpy().astype(dtype=np.int32)
+        cor_mat = obtain_correlations_matrix_train(topics_list, contents_list)
 
     one_locations = np.where(cor_mat == 1)
     zero_locations = np.where(cor_mat == 0)
-    stopics = list(one_locations[0])
-    scontents = list(one_locations[1])
+    stopics = one_locations[0]
+    scontents = one_locations[1]
 
     one_length = len(stopics)
     zero_length = int(one_length * zero_to_one_ratio)
@@ -83,8 +151,43 @@ def random_train_batch_sample(initial_sample_size = 1000, zero_to_one_ratio = No
     correlations.extend([0] * zero_length)
 
     zero_samples = list(np.random.choice(len(zero_locations[0]), zero_length, replace=False))
-    stopics.extend(zero_locations[0][zero_samples])
-    scontents.extend(zero_locations[1][zero_samples])
+    stopics = np.concatenate([stopics, zero_locations[0][zero_samples]])
+    scontents = np.concatenate([scontents, zero_locations[1][zero_samples]])
+
+    stopics = topics_list[stopics]
+    scontents = contents_list[scontents]
+
+    return scontents, stopics, correlations
+
+def random_test_batch_sample(initial_sample_size = 1000, zero_to_one_ratio = None):
+    contents_list = np.random.choice(len(data_vectorizer.test_contents), initial_sample_size, replace=False)
+    topics_list = np.random.choice(len(data_vectorizer.test_topics), initial_sample_size, replace=False)
+    cor_mat = obtain_correlations_matrix_test(topics_list, contents_list)
+
+    if zero_to_one_ratio is None:
+        return np.tile(contents_list, len(topics_list)), np.repeat(topics_list, len(contents_list)), cor_mat.flatten()
+
+    if initial_sample_size < 800:
+        raise Exception("Zero to one ratio can be used only if initial sample size >= 1000!")
+
+    while cor_mat.sum() < 10:
+        contents_list = np.random.choice(len(data_vectorizer.test_contents), initial_sample_size, replace=False)
+        topics_list = np.random.choice(len(data_vectorizer.test_topics), initial_sample_size, replace=False)
+        cor_mat = obtain_correlations_matrix_test(topics_list, contents_list)
+
+    one_locations = np.where(cor_mat == 1)
+    zero_locations = np.where(cor_mat == 0)
+    stopics = one_locations[0]
+    scontents = one_locations[1]
+
+    one_length = len(stopics)
+    zero_length = int(one_length * zero_to_one_ratio)
+    correlations = [1] * one_length
+    correlations.extend([0] * zero_length)
+
+    zero_samples = list(np.random.choice(len(zero_locations[0]), zero_length, replace=False))
+    stopics = np.concatenate([stopics, zero_locations[0][zero_samples]])
+    scontents = np.concatenate([scontents, zero_locations[1][zero_samples]])
 
     stopics = topics_list[stopics]
     scontents = contents_list[scontents]
--- model_vectorizer_simple.py
+++ model_vectorizer_simple.py
@@ -1,30 +1,20 @@
-import sys
-sys.path.insert(1, "/kaggle/input/learningequalityvectorizationml/")
-import data
-import data_vectorizer
-
-def transform_string_to_list(x):
-    if len(x) == 2:
-        return []
-    return [int(n) for n in x[1:-1].split(", ")]
-
-data.contents["title_vectorize"] = data.contents["title_vectorize"].apply(transform_string_to_list)
-data.contents["description_vectorize"] = data.contents["description_vectorize"].apply(transform_string_to_list)
-data.topics["title_vectorize"] = data.topics["title_vectorize"].apply(transform_string_to_list)
-data.topics["description_vectorize"] = data.topics["description_vectorize"].apply(transform_string_to_list)
+# the training steps are commented out since it requires quite a lot of GPU memory!!
 
 import tensorflow as tf
 import tensorflow.keras
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import data_vectorizer
+# import data_vectorizer_tf
 
-
-class ModelVectorizerSimple(tf.keras.Model):
-    def __init__(self, zero_to_one_ratio):
-        super(ModelVectorizerSimple, self).__init__()
+class Model(tf.keras.Model):
+    def __init__(self, zero_to_one_ratio, training_initial_sample_size=1000, steps_per_epoch=50):
+        super(Model, self).__init__()
 
         self.zero_to_one_ratio = zero_to_one_ratio
+        self.training_initial_sample_size = training_initial_sample_size
+        self.steps_per_epoch = steps_per_epoch
 
         # define layers here, input is (batch_size, string_sequence_length)
         # initial embedding layers. in below, we evaluate this with ragged tensors input (on second axis (1))
@@ -50,16 +40,19 @@ class ModelVectorizerSimple(tf.keras.Model):
         self.entropy = tf.keras.metrics.BinaryCrossentropy(name="entropy")
 
         # metrics for test set
-        self.test_accuracy = tf.keras.metrics.BinaryAccuracy(name="test_accuracy")
-        self.test_precision = tf.keras.metrics.Precision(name="test_precision")
-        self.test_recall = tf.keras.metrics.Recall(name="test_recall")
-        self.test_entropy = tf.keras.metrics.BinaryCrossentropy(name="test_entropy")
+        self.full_accuracy = tf.keras.metrics.BinaryAccuracy(name="full_accuracy", threshold=0.7)
+        self.full_precision = tf.keras.metrics.Precision(name="full_precision", thresholds=0.7)
+        self.full_recall = tf.keras.metrics.Recall(name="full_recall", thresholds=0.7)
+        self.test_precision = tf.keras.metrics.Precision(name="test_precision", thresholds=0.7)
+        self.test_recall = tf.keras.metrics.Recall(name="test_recall", thresholds=0.7)
+
+        self.num_step = 0
 
     def compile(self):
-        super(ModelVectorizerSimple, self).compile(run_eagerly=True)
+        super(Model, self).compile(run_eagerly=True)
         # loss and optimizer
         self.loss = tf.keras.losses.BinaryCrossentropy()
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.00025)
 
     def call(self, mdata):
         content_title, content_description, topic_title, topic_description = mdata
@@ -71,7 +64,7 @@ class ModelVectorizerSimple(tf.keras.Model):
         embedding_result = self.concat_layer([t1, t2, t3, t4])
         return self.dense3(self.dense2(self.dense1(self.relu1(embedding_result))))
 
-    def obtain_tensor_values(self, initial_sample_size=1000, zero_to_one_ratio=None):
+    """def obtain_tensor_values(self, initial_sample_size=1000, zero_to_one_ratio=None):
         contents_list, topics_list, correlations = data_vectorizer.random_train_batch_sample(
             initial_sample_size=initial_sample_size, zero_to_one_ratio=zero_to_one_ratio)
 
@@ -87,10 +80,42 @@ class ModelVectorizerSimple(tf.keras.Model):
 
         return content_title, content_description, topic_title, topic_description, y
 
+    def obtain_tensor_values2(self, initial_sample_size=1000, zero_to_one_ratio=None):
+        contents_list, topics_list, correlations = data_vectorizer_tf.random_train_batch_sample(
+            initial_sample_size=initial_sample_size, zero_to_one_ratio=zero_to_one_ratio)
+
+        contents_strings = data_vectorizer_tf.obtain_train_contents_vector(contents_list)
+        topics_strings = data_vectorizer_tf.obtain_train_topics_vector(topics_list)
+
+        content_title, content_description = contents_strings
+        topic_title, topic_description = topics_strings
+
+        y = tf.constant(correlations)
+
+        return content_title, content_description, topic_title, topic_description, y
+
+    def obtain_test_values(self, initial_sample_size=1000, zero_to_one_ratio=None):
+        contents_list, topics_list, correlations = data_vectorizer_tf.random_test_batch_sample(
+            initial_sample_size=initial_sample_size, zero_to_one_ratio=zero_to_one_ratio)
+
+        contents_strings = data_vectorizer_tf.obtain_train_contents_vector(contents_list)
+        topics_strings = data_vectorizer_tf.obtain_train_topics_vector(topics_list)
+
+        content_title, content_description = contents_strings
+        topic_title, topic_description = topics_strings
+
+        y = tf.constant(correlations)
+
+        return content_title, content_description, topic_title, topic_description, y
+
     def train_step(self, data):
         # feedforward + backpropragation with training set
-        content_title, content_description, topic_title, topic_description, y = self.obtain_tensor_values(1000,
-                                                                                                          self.zero_to_one_ratio)
+        if self.zero_to_one_ratio < 100:
+            content_title, content_description, topic_title, topic_description, y = self.obtain_tensor_values(
+                self.training_initial_sample_size, self.zero_to_one_ratio)
+        else:
+            content_title, content_description, topic_title, topic_description, y = self.obtain_tensor_values2(
+                self.training_initial_sample_size, self.zero_to_one_ratio)
 
         with tf.GradientTape() as tape:
             y_pred = self((content_title, content_description, topic_title, topic_description))
@@ -105,15 +130,24 @@ class ModelVectorizerSimple(tf.keras.Model):
             m.update_state(y, y_pred)
 
         # feedforward with test set for metrics
-        content_title, content_description, topic_title, topic_description, y = self.obtain_tensor_values(400)
+        if self.num_step % self.steps_per_epoch == 0:
+            content_title, content_description, topic_title, topic_description, y = self.obtain_tensor_values2(600)
 
-        y_pred = self((content_title, content_description, topic_title, topic_description))
+            y_pred = self((content_title, content_description, topic_title, topic_description))
 
-        for m in self.test_metrics:
-            m.update_state(y, y_pred)
+            for m in self.full_metrics:
+                m.update_state(y, y_pred)
 
+            content_title, content_description, topic_title, topic_description, y = self.obtain_test_values(600)
+
+            y_pred = self((content_title, content_description, topic_title, topic_description))
+
+            for m in self.test_metrics:
+                m.update_state(y, y_pred)
+        self.num_step += 1
         # Return a dict mapping metric names to current value
-        return {**{m.name: m.result() for m in self.metrics}, **{m.name: m.result() for m in self.test_metrics}}
+        return {**{m.name: m.result() for m in self.metrics}, **{m.name: m.result() for m in self.full_metrics},
+                **{m.name: m.result() for m in self.test_metrics}}"""
 
     @property
     def metrics(self):
@@ -121,4 +155,8 @@ class ModelVectorizerSimple(tf.keras.Model):
 
     @property
     def test_metrics(self):
-        return [self.test_accuracy, self.test_precision, self.test_recall, self.test_entropy]
+        return [self.test_precision, self.test_recall]
+
+    @property
+    def full_metrics(self):
+        return [self.full_accuracy, self.full_precision, self.full_recall]
