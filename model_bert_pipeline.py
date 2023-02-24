259c21ae37b802f50155e8cfff153fe373fb1e9d
Louis Chau <louis321yh@gmail.com>
2023-02-24 19:59:06 +0800

    Added training information regarding the stepup models.

--- model_bert_pipeline.py
+++ model_bert_pipeline.py
@@ -48,12 +48,14 @@ def train_model(model_name, custom_metrics = None, custom_stopping_func = None, 
 def train_model_stepup(model_name, custom_metrics = None, custom_stopping_func = None, custom_tuple_choice_sampler = None, custom_tuple_choice_sampler_overshoot = None):
     model = model_bert_fix_stepup.Model(units_size = 512)
     modeldir = config.training_models_path + model_name
+    checkpoint_file = modeldir + "/{epoch:07d}.ckpt"
+    logging_file = modeldir + "/logfile.csv"
 
     training_sampler = model_bert_fix.TrainingSampler(embedded_vectors_folder = config.resources_path + "bert_embedded_vectors/bert_vectorized_L6_H128/",
                                    contents_one_hot_file = config.resources_path + "one_hot_languages/contents_lang_train.npy",
                                    topics_one_hot_file = config.resources_path + "one_hot_languages/topics_lang_train.npy", device = "cpu")
     print("postsampler")
-    model.compile()
+    model.compile(weight_decay = 0.0005)
     if custom_metrics is None:
         custom_metrics = model_bert_fix_stepup.default_metrics
     if custom_stopping_func is None:
@@ -65,32 +67,33 @@ def train_model_stepup(model_name, custom_metrics = None, custom_stopping_func =
         os.mkdir(modeldir + "/")
 
     ctime = time.time()
-    checkpoint_file = modeldir + "/{epoch:07d}.ckpt"
-    logging_file = modeldir + "/logfile.csv"
-    callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_file, save_weights_only = False, verbose = 0, save_freq = 20)
+
+    callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_file, save_weights_only = False, verbose = 0, save_freq = 25)
     csv_logger = tf.keras.callbacks.CSVLogger(logging_file, separator=',', append=False)
 
-    hist = model.fit(np.array([1, 2]), epochs = 4000, callbacks=[callback, csv_logger], verbose = 2, steps_per_epoch = 1)
+    hist = model.fit(np.array([1, 2]), epochs = 3000, callbacks=[callback, csv_logger], verbose = 2, steps_per_epoch = 1)
     ctime = time.time() - ctime
     print("finished first stepup time")
     print(ctime)
 
+    """custom_stopping_func.force_final_state()
+    model.state_is_final = True
 
     ctime = time.time()
     csv_logger = tf.keras.callbacks.CSVLogger(logging_file, separator=',', append=True)
     model.compile(weight_decay = 0.001, learning_rate = tfm.optimization.lr_schedule.LinearWarmup(
         warmup_learning_rate = 0,
-        after_warmup_lr_sched = 0.0005,
-        warmup_steps = 40
+        after_warmup_lr_sched = tf.keras.optimizers.schedules.CosineDecay(0.002, decay_steps = 7000, alpha = 0.25),
+        warmup_steps = 2500
     ))
     model.set_training_params(15000, training_sampler=training_sampler, training_max_size=75000,
                               custom_metrics=custom_metrics, custom_stopping_func=custom_stopping_func,
                               custom_tuple_choice_sampler=custom_tuple_choice_sampler,
                               custom_tuple_choice_sampler_overshoot=custom_tuple_choice_sampler_overshoot)
-    model.fit(np.array([1, 2]), initial_epoch=len(hist.history["accuracy"]),epochs=4000, callbacks=[callback, csv_logger], verbose=2, steps_per_epoch=1)
+    model.fit(np.array([1, 2]), initial_epoch=100, epochs=4000, callbacks=[callback, csv_logger], verbose=2, steps_per_epoch=1) # replace 100 with len(hist.history["accuracy"])
     ctime = time.time() - ctime
     print("finished second stepup time")
-    print(ctime)
+    print(ctime)"""
 
     model.save_weights(modeldir + "/final_epoch.ckpt")
 
@@ -106,22 +109,22 @@ tuple_choice_sampler = data_bert_sampler.MixedSampler(sampler_list = [data_bert_
 metrics = model_bert_fix.obtain_overshoot_metric_instance(tuple_choice_sampler)
 train_model("overshoot23", custom_metrics = metrics, custom_tuple_choice_sampler =  tuple_choice_sampler)"""
 
-train_model_stepup("direct_model_stepup")
+train_model_stepup("direct_model_stepup_less_decay")
 
 tuple_choice_sampler = data_bert_sampler.default_sampler_instance
 tuple_choice_sampler_overshoot = data_bert_sampler.MixedSampler(sampler_list = [data_bert_sampler.default_sampler_instance, data_bert_sampler.default_sampler_overshoot2_instance])
 metrics = model_bert_fix_stepup.obtain_overshoot_metric_instance(tuple_choice_sampler, tuple_choice_sampler_overshoot)
-train_model_stepup("overshoot2_stepup", custom_metrics = metrics, custom_tuple_choice_sampler = tuple_choice_sampler, custom_tuple_choice_sampler_overshoot = tuple_choice_sampler_overshoot)
+train_model_stepup("overshoot2_stepup_less_decay", custom_metrics = metrics, custom_tuple_choice_sampler = tuple_choice_sampler, custom_tuple_choice_sampler_overshoot = tuple_choice_sampler_overshoot)
 
 tuple_choice_sampler = data_bert_sampler.default_sampler_instance
 tuple_choice_sampler_overshoot = data_bert_sampler.MixedSampler(sampler_list = [data_bert_sampler.default_sampler_instance, data_bert_sampler.default_sampler_overshoot3_instance])
 metrics = model_bert_fix_stepup.obtain_overshoot_metric_instance(tuple_choice_sampler, tuple_choice_sampler_overshoot)
-train_model_stepup("overshoot3_stepup", custom_metrics = metrics, custom_tuple_choice_sampler = tuple_choice_sampler, custom_tuple_choice_sampler_overshoot = tuple_choice_sampler_overshoot)
+train_model_stepup("overshoot3_stepup_less_decay", custom_metrics = metrics, custom_tuple_choice_sampler = tuple_choice_sampler, custom_tuple_choice_sampler_overshoot = tuple_choice_sampler_overshoot)
 
 tuple_choice_sampler = data_bert_sampler.default_sampler_instance
 tuple_choice_sampler_overshoot = data_bert_sampler.MixedSampler(sampler_list = [data_bert_sampler.default_sampler_instance, data_bert_sampler.default_sampler_overshoot2_instance, data_bert_sampler.default_sampler_overshoot3_instance])
 metrics = model_bert_fix_stepup.obtain_overshoot_metric_instance(tuple_choice_sampler, tuple_choice_sampler_overshoot)
-train_model_stepup("overshoot23_stepup", custom_metrics = metrics, custom_tuple_choice_sampler = tuple_choice_sampler, custom_tuple_choice_sampler_overshoot = tuple_choice_sampler_overshoot)
+train_model_stepup("overshoot23_stepup_less_decay", custom_metrics = metrics, custom_tuple_choice_sampler = tuple_choice_sampler, custom_tuple_choice_sampler_overshoot = tuple_choice_sampler_overshoot)
 
 """del model
 del training_sampler
